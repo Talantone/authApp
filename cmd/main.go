5ea3a723a8ca3915f7d5d334a9ad5cbf54e22c65
Talantone <talgatsitd@gmail.com>
2021-10-07 21:24:29 +0500

    working server

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,16 @@
 package main
 
-import "github.com/Talantone/authApp"
+import (
+	"github.com/Talantone/authApp"
+	"github.com/Talantone/authApp/pkg/handler"
+	"log"
+)
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(authApp.Server)
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+		log.Fatalf("Error occured while running http server: %s", err.Error())
+	}
 
 }
---
+++ user.go
@@ -0,0 +1,8 @@
+package authApp
+
+type User struct {
+	Id       int    `json:"-"`
+	Name     string `json:"name"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
